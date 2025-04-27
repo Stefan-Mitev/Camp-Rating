6c2d949b4ac7a8729c6283ee034f74c304a7301c
Stefan-Mitev <123822245+Stefan-Mitev@users.noreply.github.com>
2025-04-27 10:34:28 +0300

    commit 1

---
+++ Camp Rating/Models/Campsite.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Camp_Rating.Models
+{
+    [Table("Campsite")]
+    public class Campsite
+    {
+        [Required]
+        [MaxLength(200)]
+        public string? BookName
+        {
+            get; set;
+        }
+
+    }
+}
